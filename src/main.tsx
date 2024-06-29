7c599bdf1bb35a5ea0d1e5070973ff85d9a37e92
JunyuWang <happystyle666@gmail.com>
2024-06-29 21:45:02 +0800

    create chat app and install react router dom package

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.tsx'
+import Login from './pages/Login.tsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/home',
+    element: <Home />
+  },
+  {
+    path: '*',
+    element: <Login />
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+    </RouterProvider>
+  </React.StrictMode>,
+)
---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Login() {
+    return (
+        <>
+            <Link to={'home'}>go to Home</Link>
+            <div>Login</div>
+        </>
+    )
+}
+
+export default Login
