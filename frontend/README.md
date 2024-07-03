576bbbaa3e1462051f746a18e291833f8fbfccf2
JunyuWang <happystyle666@gmail.com>
2024-07-03 23:51:29 +0800

    (refactor) install reacthookform as form feature and update README

--- frontend/README.md
+++ frontend/README.md
@@ -42,4 +42,53 @@ const submit = (e: React.FormEvent<HTMLFormElement>) => {
     }
 ```
 
-- 使用 reacthookform 取資料的方式
+- 使用 [reacthookform](https://www.react-hook-form.com/) 取資料的方式
+
+    `npm install react-hook-form`
+  
+    這個 hook 太棒了，完全可以省略掉不必要的迴圈
+
+    .ts 檔案內只是單純用了 `useForm`，且宣告的一個 `interface` 和 `defaultValues`
+
+    ```ts
+    interface RegisterProps {
+    text: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+
+    }
+
+    const defaultValues: RegisterProps = {
+        text: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+    const { register, handleSubmit } = useForm({ defaultValues });
+
+    const submit = (data: RegisterProps) => {
+        console.log(data);
+        // e.preventDefault();
+        // const formElements = e.currentTarget.elements;
+        // const formData: { [key: string]: string } = {};
+        // for (let i = 0; i < formElements.length; i++) {
+        //     const input = formElements[i] as HTMLInputElement;
+        //     if (input.name && input.type !== 'button' && input.type !== 'submit') {
+        //         formData[input.name] = input.value;
+        //     }
+        // }
+        // console.log(formData);
+    }
+    ```
+
+    .tsx 只要把原本寫 `name` 的地方改為 `require` 的語法就可以了
+
+    ```tsx
+        <form onSubmit={handleSubmit(submit)} >
+                    <input {...register('text')}  />
+                    <input {...register('email')}  />
+                    <input {...register('password')}  />
+                    <input {...register('confirmPassword')}  />
+                    </form>
+    ```
--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,31 +1,50 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import style from './Register.module.scss';
+import { useForm } from 'react-hook-form';
+
+interface RegisterProps {
+    text: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+
+}
+
+const defaultValues: RegisterProps = {
+    text: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
+
 
 function Register() {
+    const { register, handleSubmit } = useForm({ defaultValues });
     const redirect = useNavigate();
 
-    const submit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const formElements = e.currentTarget.elements;
-        const formData: { [key: string]: string } = {};
-        for (let i = 0; i < formElements.length; i++) {
-            const input = formElements[i] as HTMLInputElement;
-            if (input.name && input.type !== 'button' && input.type !== 'submit') {
-                formData[input.name] = input.value;
-            }
-        }
-        console.log(formData);
+    const submit = (data: RegisterProps) => {
+        console.log(data);
+        // e.preventDefault();
+        // const formElements = e.currentTarget.elements;
+        // const formData: { [key: string]: string } = {};
+        // for (let i = 0; i < formElements.length; i++) {
+        //     const input = formElements[i] as HTMLInputElement;
+        //     if (input.name && input.type !== 'button' && input.type !== 'submit') {
+        //         formData[input.name] = input.value;
+        //     }
+        // }
+        // console.log(formData);
     }
 
     return (
         <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center gap-3">
             <p className='text-white'>🚀 <strong>Welcome to Chap App</strong></p>
-            <form onSubmit={submit} className="flex flex-col justify-center items-center gap-8 bg-opacity-80 bg-black rounded-3xl px-16 py-12">
-                <input className={style.input} type='text' name='text' placeholder='UserName' />
-                <input className={style.input} type='email' name='email' placeholder='Email' />
-                <input className={style.input} type='password' name='password' placeholder='Password' />
-                <input className={style.input} type='password' name='confirmPassword' placeholder='Confirm Password' />
+            <form onSubmit={handleSubmit(submit)} className="flex flex-col justify-center items-center gap-8 bg-opacity-80 bg-black rounded-3xl px-16 py-12">
+                <input className={style.input} type='text' {...register('text')} placeholder='UserName' />
+                <input className={style.input} type='email' {...register('email')} placeholder='Email' />
+                <input className={style.input} type='password' {...register('password')} placeholder='Password' />
+                <input className={style.input} type='password' {...register('confirmPassword')} placeholder='Confirm Password' />
                 <div className='text-center'>
                     <button type="submit" className='p-2 rounded-md bg-primary text-white w-full hover:bg-[#4f58ff] transition ease-in-out duration-700'>Create Account</button>
                     <span className='text-white'>or</span>
