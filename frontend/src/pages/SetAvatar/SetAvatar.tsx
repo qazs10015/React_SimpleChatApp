51673d1b5634b09f39c670fc360caf3c97e98850
JunyuWang <happystyle666@gmail.com>
2024-07-12 18:25:55 +0800

    add Referch and setAvatar button feature

--- frontend/src/pages/SetAvatar/SetAvatar.tsx
+++ frontend/src/pages/SetAvatar/SetAvatar.tsx
@@ -15,20 +15,39 @@ async function avatarPromise() {
 
 function SetAvatar() {
 
-    const [avatars, setAvatars] = useState<string[]>([]);
+    const [avatarList, setAvatarList] = useState<string[]>([])
+
+    const [avatar, setAvatar] = useState<string>('');
+
+
+    const refresh = async () => {
+        setAvatarList((await avatarPromise()).map(res => res.data));
+    }
 
     useEffect(() => {
-        // just fetch img data via IIFE, no need declare variable
-        (async () => setAvatars((await avatarPromise()).map(res => res.data)))();
+        refresh();
     }, []);
+
+    const pickAvatar = (avatar: string) => setAvatar(avatar);
 
     return (
         <>
-            <div className='text-white text-4xl'>Choose your favorite avatar</div>
-            {avatars.map((avatar, index) => {
-                // encode svg info to base64 and show it
-                return <img key={index} src={`data:image/svg+xml;base64,${btoa(avatar)}`}></img>
-            })}
+            <div className='text-white text-center text-4xl mb-4'>Choose Your Favorite Avatar</div>
+            <div className='flex flex-col justify-center items-center gap-6'>
+                <div className='flex gap-10'>
+                    {avatarList.map((item, index) => {
+                        // encode svg info to base64 and show it
+                        return (
+                            <img className={`w-[120px] h-[120px] rounded-full cursor-pointer p-1 border-4 border-transparent hover:border-yellow-400 duration-700 ease-out ${avatar === btoa(item) && 'border-yellow-400'}`} key={index} src={`data:image/svg+xml;base64,${btoa(item)}`} onClick={() => pickAvatar(btoa(item))}></img>
+                        )
+                    })}
+                </div>
+                <div className='flex gap-4'>
+                    <button className='p-2 w-[5rem] rounded-md bg-white text-primary hover:bg-secondary hover:text-white transition ease-in-out duration-700' onClick={() => refresh()}>Refresh</button>
+                    <button className='p-2 w-[5rem] rounded-md bg-primary text-white hover:bg-secondary hover:text-white transition ease-in-out duration-700' onClick={() => refresh()}>Pick it!</button>
+                </div>
+            </div>
+
 
         </>
     )
