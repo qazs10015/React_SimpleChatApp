ee3d6e0e5da940316ce45f96c067cca9fe279994
JunyuWang <happystyle666@gmail.com>
2024-08-03 14:50:51 +0800

    added test api

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
-app.get('/', (req, res) => {
+app.get('/api/test', (req, res) => {
     res.send('Hello World!');
 });
 
